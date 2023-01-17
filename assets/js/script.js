b50190d5908ea3c0a01d4390ac79a01f9b999c68
rasmus1111 <rasmusareaxelsson@gmail.com>
2023-01-17 08:56:22 +0000

    Add code to calculate two random numbers for the game quistions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
     let buttens = document.getElementsByTagName("button");
 
-        for ( let button of buttens){
-            button.addEventListener("click", funtion()){
-                if (this.getAttribute("data-type") === "submit"){
+    for ( let button of buttens){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
                     alert("you clicked Submit");
-                }else {
+            }else {
                     let gameType = this.getAttribute("data-type");
                     alert(`you clicked ${gameType}`);
-                }
             }
-
-
-        }
-    
-
-   
+        })
+    }
 })
 
-function runGame( ){
+/**
+ * The main game "loop", ccalled when the script is first loaded
+ * and after the user's answer has been processed
+ */
 
+function runGame( ){
+   let num1 = Math.floor(Math.random() * 25 ) + 1;
+   let num2 = Math.floor(Math.random() * 25 ) + 1;
 }
+
 
 function checkAnswer(){
 
