fcf9350280699e1d60393e2fe0d90c85c137aa5b
rasmus1111 <rasmusareaxelsson@gmail.com>
2023-01-18 05:08:10 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
+    runGame("addition")
 })
 
 /**
@@ -18,9 +19,16 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 
-function runGame( ){
+function runGame(gameType){
    let num1 = Math.floor(Math.random() * 25 ) + 1;
    let num2 = Math.floor(Math.random() * 25 ) + 1;
+
+   if (gameType === "addition"){
+    displayAdditionQuestion(num1, num2);
+   }else{
+    alert(`Unknown game type: ${gameType} `);
+    throw `Unknown game type: ${gameType}.Aborting`;
+   }
 }
 
 
@@ -40,7 +48,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 function displaySubtractQuestion(){
